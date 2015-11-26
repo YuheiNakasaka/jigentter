cd7f4f1c7568bccb7eb3ebdffb8428daf8412737
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2015-11-26 15:32:21 +0900

    js,css digest and compress

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,10 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = true
   config.serve_static_assets = true
-
-
+  config.assets.js_compressor = :uglifier
+  config.assets.compile = false
+  config.assets.digest = true
+  config.assets.version = '1.0'
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
